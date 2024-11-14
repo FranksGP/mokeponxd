bf87aa3699e18a42e6ebcdd32e7f344b5a0fdd63
FranksGP <fsebastian.garzong@gmail.com>
2024-11-14 01:08:20 -0500

    Se creo la funcion seleccionMascotaJugador, seleccionMascotaEnemigo y Aleatorio en el archivo mokepon.js se creo la variables 'input' en la función seleccionMascotaJugador, llama a los elementos de las mascotas del DOM Se creo la variable spanMascotaJugador, llama al elemento 'mascota-jugador' del DOM Se creo el condicional en la funcion seleccionMacotajugador que chequea si se eligio o no a la mascota, si la condicion es cierta, edita el archibo html y pone el nombre de la mascota seleccionada, si la condicion no se cumple va a arrojar una alerta diciendo 'No has seleccionado una mascota' Se creo la variable AtaqueAleatorio en la funcion seleccionarMascotaEnemigo se creo la variable spanMascotaEnemigo en la funcion selecionarMascotaEnemigo se creo el condicional para chechear la elecion del enemigo en la funcion seleccionarMascotaEnemigo se llamo la funcion seleccionMascotaEnemigo en la funcion seleccionMascotaJugador

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,11 +1,40 @@
-// alert("hola mundo js")
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascoraJuagador)
 }
 
 function seleccionarMascoraJuagador(){
-    alert("seleccionaste tu mascotaaa")
+    let inputHipodogue = document.getElementById('hipodoge')//.getElementById es un metodo que nos permite acceder a un elemento del DOM
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+
+    if(inputHipodogue.checked){ //.checked es un metodo que funciona para verificar que elemento es seleccionado
+        spanMascotaJugador.innerHTML = 'Hipodoge'
+    } else if (inputCapipepo.checked){
+        spanMascotaJugador.innerHTML = 'Capipepo'
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } else{
+        alert('No has seleccionado una mascota')
+    }
+
+    seleccionarMascoraEnemigo()
+}
+function seleccionarMascoraEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio == 1){
+        spanMascotaEnemigo.innerHTML = 'Hipodogue'
+    } else if (ataqueAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+function aleatorio(min, max){
+    return Math.floor( Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener('load', iniciarJuego)
